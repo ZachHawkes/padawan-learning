8e1ce16437e14945f7a4c16fbd3ee32519fe1758
Tanner Palmer <tannerp@ifit.com>
2021-04-15 14:02:18 -0600

    solved adding elements function

--- exercises/strings-and-objects.js
+++ exercises/strings-and-objects.js
@@ -6,5 +6,16 @@
 // two strings hello, world should return "hello world"
 // two number should 1, 3 should return 4
 function add(x, y) {
+    const typeOfX = typeof x;
+    const typeOfY = typeof y;
+    if (typeOfX === 'string' || typeOfY === 'string') {
+        console.log(`${x} ${y}`);
+        //console.log(x + " " + y);
+    }
+    else {
+        console.log(x + y);
+    }
 
 }
+
+add();
