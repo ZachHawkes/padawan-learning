c0a47943f3c5d9e260157b7615a918f4948b9c15
Tanner Palmer <tannerp@ifit.com>
2021-04-22 13:16:56 -0600

    reverseString

--- exercises/strings-and-objects.js
+++ exercises/strings-and-objects.js
@@ -1,27 +1,23 @@
-
-
-
 // add two elements together.
 // elements could be strings or numbers
 // two strings hello, world should return "hello world"
 // two number should 1, 3 should return 4
 function add(x, y) {
-    const typeOfX = typeof x;
-    const typeOfY = typeof y;
-    if (typeOfX === 'string' || typeOfY === 'string') {
-        // console.log(`${x} ${y}`);
-        // console.log(x + " " + y);
-        return `${x} ${y}`;
-    }
-    else {
-        // console.log(x + y);
-        return x + y;
-    }
+  const typeOfX = typeof x;
+  const typeOfY = typeof y;
+  if (typeOfX === "string" || typeOfY === "string") {
+    // console.log(`${x} ${y}`);
+    // console.log(x + " " + y);
+    return `${x} ${y}`;
+  } else {
+    // console.log(x + y);
+    return x + y;
+  }
 }
 
 function addVersion2(x, y) {
-  if(!isNaN(x) || !isNaN(y)) {
-    return `${x} ${y}`; 
+  if (!isNaN(x) || !isNaN(y)) {
+    return `${x} ${y}`;
   }
   return x + y;
 }
@@ -32,8 +28,16 @@ function addVersion2(x, y) {
 // return the string in reverse order ie "hello" => "olleh";
 // HINT: Convert the string to an array
 function reverseString(str) {
+  //const array = str.split('');
+  str.split("");
+  for (let i = str.length - 1; i >= 0; i--) {
+    console.log(str[i]);
 
+    //return (str[i]);
+  }
 }
+
+console.log(reverseString("Hello"));
 
 // return true if the property is on the provided obj
 // return false if the property is not on the provided obj
@@ -42,10 +46,10 @@ function isPropertyInObject(property, obj) {
 }
 
 const person = {
-  firstName: 'Mario',
-  lastName: 'Smith',
+  firstName: "Mario",
+  lastName: "Smith",
   phoneNumber: 7,
-  occupation: 'Plumber',
+  occupation: "Plumber",
 };
 
-console.log(isPropertyInObject('occupation', person));
+console.log(isPropertyInObject("occupation", person));
