165babece817a44dcd1c3779b763def87fdd7f5f
Zach Hawkes <zach@ifit.com>
2021-04-22 14:38:44 -0600

    worked on reverse string function
    Co-authored-by: Tannerp647 <Tannerp647@users.noreply.github.com>

--- exercises/strings-and-objects.js
+++ exercises/strings-and-objects.js
@@ -3,16 +3,14 @@
 // two strings hello, world should return "hello world"
 // two number should 1, 3 should return 4
 function add(x, y) {
-  const typeOfX = typeof x;
-  const typeOfY = typeof y;
-  if (typeOfX === "string" || typeOfY === "string") {
-    // console.log(`${x} ${y}`);
-    // console.log(x + " " + y);
-    return `${x} ${y}`;
-  } else {
-    // console.log(x + y);
-    return x + y;
-  }
+    const typeOfX = typeof x;
+    const typeOfY = typeof y;
+    if (typeOfX === 'string' || typeOfY === 'string') {
+        return `${x} ${y}`;
+    }
+    else {
+        return x + y;
+    }
 }
 
 function addVersion2(x, y) {
@@ -28,17 +26,16 @@ function addVersion2(x, y) {
 // return the string in reverse order ie "hello" => "olleh";
 // HINT: Convert the string to an array
 function reverseString(str) {
-  //const array = str.split('');
-  //str.split("");
-  String(str);
+  let answer = "";
   for (let i = str.length - 1; i >= 0; i--) {
-    console.log(str[i]);
-
-    //return (str[i]);
+    answer = answer + str[i];
   }
+  return answer;
+  // Option 2:
+  // const stringArray = str.split('');
+  // stringArray.reverse();
+  // return stringArray.join('');
 }
-
-reverseString("Hello");
 
 // return true if the property is on the provided obj
 // return false if the property is not on the provided obj
@@ -46,11 +43,9 @@ function isPropertyInObject(property, obj) {
   return Boolean(obj[property]);
 }
 
-const person = {
-  firstName: "Mario",
-  lastName: "Smith",
-  phoneNumber: 7,
-  occupation: "Plumber",
+module.exports = {
+  add,
+  addVersion2,
+  reverseString,
+  isPropertyInObject,
 };
-
-console.log(isPropertyInObject("occupation", person));
