5416f4a8e67833860f556601fb8e6c4318a15cd9
Tanner Palmer <tannerp@ifit.com>
2021-04-29 09:52:07 -0600

    add two strings

--- exercises/strings-and-objects.js
+++ exercises/strings-and-objects.js
@@ -54,3 +54,20 @@ const person = {
 };
 
 console.log(isPropertyInObject("occupation", person));
+
+// two strings and add them together as numbers.
+function addStrings(str1, str2) {
+  let answer = 0;
+  answer = parseInt(str1) + parseInt(str2);
+  //console.log(answer);
+  return answer;
+}
+
+addStrings("23", "45");
+
+// two strings add together as numbers a little more efficient
+function addStrings2(str1, str2) {
+  return parseInt(str1) + parseInt(str2);
+}
+
+console.log(5 + addStrings2("3", "2"));
